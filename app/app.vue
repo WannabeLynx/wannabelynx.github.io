26fe5e4a603580c051ff79f7844eed929522a9c5
Rory <wannabelynx@web.de>
2025-10-20 10:00:44 +0200

    Update app.vue

--- app/app.vue
+++ app/app.vue
@@ -1,9 +1,9 @@
 <template>
-  <div className="min-h-screen bg-[#050810] overflow-x-hidden">
-    <div className="fixed inset-0 z-0">
-      <div className="absolute top-0 left-1/4 w-96 h-96 bg-[#64FFDA]/5 rounded-full blur-3xl" />
-      <div className="absolute top-1/3 right-1/4 w-96 h-96 bg-[#E040FB]/5 rounded-full blur-3xl" />
-      <div className="absolute bottom-1/4 left-1/3 w-96 h-96 bg-[#FFC107]/5 rounded-full blur-3xl" />
+  <div class="min-h-screen bg-[#050810] overflow-x-hidden">
+    <div class="fixed inset-0 z-0">
+      <div class="absolute top-0 left-1/4 w-96 h-96 bg-[#64FFDA]/5 rounded-full blur-3xl" />
+      <div class="absolute top-1/3 right-1/4 w-96 h-96 bg-[#E040FB]/5 rounded-full blur-3xl" />
+      <div class="absolute bottom-1/4 left-1/3 w-96 h-96 bg-[#FFC107]/5 rounded-full blur-3xl" />
     </div>
     <NuxtPage />
   </div>
